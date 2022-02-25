86d717f600af37b04a7514bfc0af769c19ce0ef6
Maksim Lopatin <maksim.lopatin.spb@gmail.com>
2022-02-25 11:07:45 +0300

    3 секунды на чтение большого файла

--- testlib.py
+++ testlib.py
@@ -1,36 +1,37 @@
 import csiread
 from csiread import utils
-from time import time
+from time import time, sleep
 from matplotlib import pyplot as plt
 
-
-# filepathes = [
-#   './csi_data/homelocation/r_oomk_itchent_oilet/train/kitchen.dat',
-#   './csi_data/homelocation/r_oomk_itchent_oilet/train/room.dat',
-#   './csi_data/homelocation/r_oomk_itchent_oilet/train/toilet.dat',
-#   './csi_data/homelocation/r_oomk_itchent_oilet/test/kitchen.dat',
-#   './csi_data/homelocation/r_oomk_itchent_oilet/test/room.dat',
-#   './csi_data/homelocation/r_oomk_itchent_oilet/test/toilet.dat',
-# ]
-
-
-
-
+filepathes = [
+  './csi_data/homelocation/r_oomk_itchent_oilet/train/kitchen.dat',
+  './csi_data/homelocation/r_oomk_itchent_oilet/train/room.dat',
+  './csi_data/homelocation/r_oomk_itchent_oilet/train/toilet.dat',
+  './csi_data/homelocation/r_oomk_itchent_oilet/test/kitchen.dat',
+  './csi_data/homelocation/r_oomk_itchent_oilet/test/room.dat',
+  './csi_data/homelocation/r_oomk_itchent_oilet/test/toilet.dat',
+]
 
 csifile = "./csi_data/homelocation/r_oomk_itchent_oilet/train/room.dat"
-csidata = csiread.Atheros(csifile, nrxnum=2, ntxnum=5, tones=56, if_report=False)
-
+csidata = []
 start = time()
-csidata.read(endian='big')
+for path in filepathes:
+  data = csiread.Atheros(csifile, nrxnum=2, ntxnum=5, tones=56, if_report=False)
+  data.read(endian='big')
+  csidata.append(data)
 
+print(time() - start)
+sleep(30)
+exit()
 print(csidata.csi.shape)
 csi = csidata.csi[:100]
 phase0 = utils.np.unwrap(utils.np.angle(csi), axis=1)
 phase = utils.calib(phase0, k=utils.scidx(20, 1), axis=1)
+phaseTest = utils.np.angle(csi)
 
 for i in [9, 18, 29]:
   plt.plot(phase0[i][:, 1, 1])
   plt.plot(phase[i][:, 1, 1])
   plt.grid()
-  print(i, phase0[i][:, 1, 1])
+  print(i, phase0[i][:, 1, 1], phaseTest[i][:, 1, 1])
   plt.show()
