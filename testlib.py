baadafdd48e33423063c8d98429548ad0edd9e3d
Maksim Lopatin <maksim.lopatin.spb@gmail.com>
2022-02-25 10:55:52 +0300

    Почему np.angle выдает значения больше 2pi

--- testlib.py
+++ testlib.py
@@ -3,26 +3,34 @@ from csiread import utils
 from time import time
 from matplotlib import pyplot as plt
 
-print('hi')
-# Atheros CSI Tool
+
+# filepathes = [
+#   './csi_data/homelocation/r_oomk_itchent_oilet/train/kitchen.dat',
+#   './csi_data/homelocation/r_oomk_itchent_oilet/train/room.dat',
+#   './csi_data/homelocation/r_oomk_itchent_oilet/train/toilet.dat',
+#   './csi_data/homelocation/r_oomk_itchent_oilet/test/kitchen.dat',
+#   './csi_data/homelocation/r_oomk_itchent_oilet/test/room.dat',
+#   './csi_data/homelocation/r_oomk_itchent_oilet/test/toilet.dat',
+# ]
+
+
+
+
+
 csifile = "./csi_data/homelocation/r_oomk_itchent_oilet/train/room.dat"
 csidata = csiread.Atheros(csifile, nrxnum=2, ntxnum=5, tones=56, if_report=False)
 
 start = time()
-for i in range(1):
-  csidata.read(endian='big')
-
-print('time:', time() - start)
+csidata.read(endian='big')
 
 print(csidata.csi.shape)
-print(csidata.csi[0])
-print('hello')
-
-csi = csidata.csi[:10]
+csi = csidata.csi[:100]
 phase0 = utils.np.unwrap(utils.np.angle(csi), axis=1)
 phase = utils.calib(phase0, k=utils.scidx(20, 1), axis=1)
-print(phase[0][:, 1, 1])
-plt.plot(phase0[0][:, 1, 1])
-plt.plot(phase[0][:, 1, 1])
-plt.grid()
-plt.show()
+
+for i in [9, 18, 29]:
+  plt.plot(phase0[i][:, 1, 1])
+  plt.plot(phase[i][:, 1, 1])
+  plt.grid()
+  print(i, phase0[i][:, 1, 1])
+  plt.show()
