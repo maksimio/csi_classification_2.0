37328616a19f9bc86431f40eb03de0163bcae6d2
Maksim L <maksim.lopatin.spb@gmail.com>
2022-02-26 19:34:56 +0300

    Пишу csitools

--- csitools/processing/__init__.py
+++ csitools/processing/__init__.py
@@ -1,14 +1,23 @@
 import numpy as np
+import csiread
 
 # Преобразование к виду 56 x 4 - амплитуды или фазы
-def reshape56x4(arr):
+def reshape56x4(csi: np.ndarray):
+  # csi = csi.reshape()
   pass
 
 # Преобразование к виду 224 x 1 - амплитуды или фазы
-
-
-# Преобразование к виду 224 x 2 - амплитуды и фазы
-
+def reshape224x1(arr):
+  pass
 
 # Нарезка по первой размерности по заданному чанку (по умолчанию - квадрат).
 # Неполный чанк в конце отбрасывается
+def chunks(arr):
+  pass
+
+def extractAm(csi: np.ndarray):
+  return np.abs(csi)
+
+def extractPh(csi: np.ndarray):
+  ph = np.unwrap(np.angle(csi), axis=1)
+  return csiread.utils.calib(ph, k=csiread.utils.scidx(20, 1), axis=1)
--- writecsitools.py
+++ writecsitools.py
@@ -1,2 +1,8 @@
-import csitools
+import csitools.processing as csip
+import csitools.reading as csir
+import csiread
+
+path = './csi_data/use_in_paper/2_objects/train/D=2020-01-02_T=21-09-35--air.dat'
+csi = csir.extractCSI(path)
+am = csip.extractAm(csi)
 
