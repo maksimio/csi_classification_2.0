135f51b300160e31741769825973bb548ef02f46
Maksim L <maksim.lopatin.spb@gmail.com>
2022-02-26 20:36:00 +0300

    Чистка

--- torchtest.py
+++ torchtest.py
@@ -7,7 +7,6 @@ import numpy as np
 
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 
 def imshow(img):
     img = img / 2 + 0.5
--- writecsitools.py
+++ writecsitools.py
@@ -1,12 +1,13 @@
 import csitools.processing as csip
 import csitools.reading as csir
 import csitools.viewing as csiv
+import csitools
 import numpy as np
 
 path = './csi_data/use_in_paper/2_objects/train/D=2020-01-02_T=21-09-35--air.dat'
 csi = csir.extractCSI(path)
+csi = csip.extractAm(csi)
 csi = csip.reshape4x56(csi)
-csi = csip.extractPh(csi)
 csi = np.diff(csi)
 csi = np.reshape(csi, (csi.shape[0], -1))
 csi = csip.down(csi)
