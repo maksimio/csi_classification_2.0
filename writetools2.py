90532425477fe71fffaf26a74da118ce0b1e5daf
Maksim L <maksim.lopatin.spb@gmail.com>
2022-02-27 01:01:10 +0300

    Написал чанки

--- writetools2.py
+++ writetools2.py
@@ -8,11 +8,22 @@ import numpy as np
 
 categories = ['air', 'bottle']
 dirpath = './csi_data/use_in_paper/2_objects/train'
+frameHeight = 50
+
 for fname in listdir(dirpath):
   csi = csir.extractCSI(dirpath + '/' + fname)
   csi = csip.extractAm(csi)
   csi = csip.reshape4x56(csi)
-  csi = np.diff(csi)
-  csi = np.reshape(csi, (csi.shape[0], -1))
-  csi = csip.down(csi)
+  # csi = np.diff(csi)
+
+  csi = np.expand_dims(csi, axis=0)
+  print(csi.shape)
+  csi = csi[:,:csi.shape[1]//frameHeight*frameHeight,:,:]
+  print(csi.shape)
+  csi = np.reshape(csi, (-1, frameHeight, 4, csi.shape[3]))
+  print(csi.shape)
+  csi = np.transpose(csi, (0,2,1,3))
+  print(csi.shape)
+  break
+  # csi = csip.down(csi)
   print(csi.shape, csir.match(categories, fname))
